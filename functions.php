6cac415e6c4855d07f7082f5a7ccebfd0a2b1b46
raky291 <raky291@gmail.com>
2019-07-10 16:49:30 -0600

    add child theme scripts

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+
+defined('ABSPATH') || exit;
+
+define('__R9_UNDERSTRAP_DIR__', get_stylesheet_directory());
+define('__R9_UNDERSTRAP_URL__', get_stylesheet_directory_uri());
+
+require_once __R9_UNDERSTRAP_DIR__ . '/includes/class-r9-understrap.php';
---
+++ includes/class-r9-understrap.php
@@ -0,0 +1,60 @@
+<?php
+
+defined('ABSPATH') || exit;
+
+class R9_Understrap
+{
+    public function __construct()
+    {
+        add_action('wp_enqueue_scripts', array($this, 'add_scripts'));
+        add_action('wp_enqueue_scripts', array($this, 'remove_scripts'), 20);
+        add_action('wp_enqueue_scripts', array($this, 'move_scripts'), 30);
+    }
+
+    public function add_scripts()
+    {
+        $this->r9_enqueue_style('r9-understrap-styles', '/assets/dist/css/child-theme.css');
+
+        $this->r9_enqueue_script('r9-understrap-vendors', '/assets/dist/js/child-theme.vendors.js', array('jquery'));
+        $this->r9_enqueue_script('r9-understrap-main', '/assets/dist/js/main.js', array('jquery'));
+    }
+
+    public function remove_scripts()
+    {
+        wp_dequeue_style('understrap-styles');
+        wp_deregister_style('understrap-styles');
+
+        wp_dequeue_script('understrap-scripts');
+        wp_deregister_script('understrap-scripts');
+    }
+
+    public function move_scripts()
+    {
+        wp_script_add_data('jquery', 'group', 1);
+        wp_script_add_data('jquery-core', 'group', 1);
+        wp_script_add_data('jquery-migrate', 'group', 1);
+    }
+
+    private function r9_enqueue_style($handle, $src)
+    {
+        wp_enqueue_style(
+            $handle,
+            __R9_UNDERSTRAP_URL__ . $src,
+            array(),
+            filemtime(__R9_UNDERSTRAP_DIR__ . $src)
+        );
+    }
+
+    private function r9_enqueue_script($handle, $src, $deps)
+    {
+        wp_enqueue_script(
+            $handle,
+            __R9_UNDERSTRAP_URL__ . $src,
+            $deps,
+            filemtime(__R9_UNDERSTRAP_DIR__ . $src),
+            true
+        );
+    }
+}
+
+new R9_Understrap();
