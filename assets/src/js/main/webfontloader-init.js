c7e8b145f1ae3bbdc48057304b5c4242f2d3c0bd
raky291 <raky291@gmail.com>
2019-08-06 10:08:28 -0600

    add default font family

--- assets/src/js/main/webfontloader-init.js
+++ assets/src/js/main/webfontloader-init.js
@@ -1,5 +1,5 @@
 WebFont.load({
     google: {
-        families: ['Montserrat']
+        families: ['Montserrat:300,300i,400,400i,500,500i,700,700i']
     }
 });
--- includes/class-r9-understrap.php
+++ includes/class-r9-understrap.php
@@ -14,7 +14,7 @@ class R9_Understrap
     public function add_scripts()
     {
         $this->r9_enqueue_style('r9-understrap-styles', '/assets/dist/css/child-theme.css');
-        $this->r9_enqueue_script('r9-understrap-vendors', '/assets/dist/js/child-theme.vendors.js', array('jquery'));
+        $this->r9_enqueue_script('r9-understrap-main', '/assets/dist/js/child-theme.js', array('jquery'));
     }
 
     public function remove_scripts()
