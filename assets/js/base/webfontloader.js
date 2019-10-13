ade84e64828c5beecff3103e7b52f5c5369e71d2
raky291 <raky291@gmail.com>
2019-10-12 23:50:24 -0600

    remove google font italic

--- assets/js/base/webfontloader.js
+++ assets/js/base/webfontloader.js
@@ -2,6 +2,6 @@ import WebFont from 'webfontloader';
 
 WebFont.load({
     google: {
-        families: ['Montserrat:300,300i,400,400i,500,500i,700,700i']
+        families: ['Montserrat:300,400,500,700']
     }
 });
